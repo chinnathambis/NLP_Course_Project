fe06343179a297b4b5aa2e6a1b2a6937d421ffca
Chinnathambi S <m66l4n3@gmail.com>
2016-04-18 23:08:09 +0530

    Done Seventh Question

---
+++ question7/q7_a/q7_a.py
@@ -0,0 +1,60 @@
+from itertools import islice
+from itertools import izip
+import collections
+import re
+
+
+def vocabulary(filename):
+    
+	f1 = open(filename, "r")
+	f1.seek(0,0)
+	file_data = f1.read() 
+	words = file_data.split()
+	count = data_file.Counter()
+	
+	for word in words:
+		count[word] = count[word] + 1
+
+
+	v = open("vocabulary.txt","w")
+	for word in count:
+		if count[word] >= 3:
+			v.write(word+"\n")	
+
+	v.close()
+	f1.close()
+
+
+
+def bigram(filename):
+    
+	inFile = open(filename,"r")
+	dataset_content = inFile.read()
+	words = dataset_content.split()
+
+	create_bigrams = collections.Counter(izip(words, islice(words,1,None)))
+
+	inFile.close()
+
+	
+	inFile = open("vocabulary.txt","a+")
+	for word in create_bigrams:
+		if create_bigrams[word] >= 3:
+			inFile.write(" ".join(word))
+			inFile.write("\n")
+			
+	v = open("new_vocabulary.txt","w")
+	
+	inFile.seek(0,0)
+	for line in inFile:
+            
+		if(not line.startswith('+') and not line.startswith('-') and not line[-2:-1] == '+' and not line[-2:-1] == '-'):
+#               if(not line[2:1] == '+' and not line[2:1] == '-'and not line.startswith('+') and not line.startswith('-')):
+			v.write(line)
+	inFile.close()
+	v.close()
+
+	#print "successful...!"
+
+
+bigram("trainingdata.txt")
---
+++ question7/q7_b/q5_b.cpp
@@ -0,0 +1,128 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include<iomanip>
+using namespace std;
+double totalpve=1,totalnve=1;
+vector<string> split(string str){
+	vector<string> words;
+	string token="";
+
+	int i=2;
+	while(i<str.length()){
+		if(str[i]!=' ')
+			token.push_back(str[i]);
+		else{
+			if(token.length()>0 && token[0]>='a' && token[0]<='z')
+				words.push_back(token);
+			token="";
+		}
+		i++;
+	}
+	if(token.length()>0 && token[0]>='a' && token[0]<='z'){
+		token.erase(token.size() - 1);
+		words.push_back(token);
+	}
+	return words;
+}
+int countWords(string str)
+{
+	int count=0;
+	int flag=0;
+	for(int i=2;i<str.size();i++)
+	{	
+		if(flag==1&&(str[i]==' '||str[i]=='\n'))
+		{
+			count++;
+			flag=0;
+		}
+		else if(isalpha(str[i]))
+		{
+			flag=1;
+		}		
+	}
+	if(flag==1)
+		count++;
+	return count;
+}
+int main()
+{
+  int v,t,p=0,n=0,pc,nc;
+
+  vector <string> voc;
+  
+  vector <string> voct;
+  vector<int> positiveCounts;
+  vector<int> negativeCounts;
+  string str;
+  string str1;
+  string str2;
+  ifstream fin("new_vocabulary.txt");
+  ifstream fn("testdata.txt");
+  while (fin>>str>>pc>>nc) 
+  {          
+        
+    	voc.push_back(str);
+	positiveCounts.push_back(pc);
+	negativeCounts.push_back(nc);
+	p+=pc;
+	n+=nc;	
+  }
+  fin.close();
+int vocsize=voc.size();
+int correct=0,wrong=0;
+
+while(getline(fn,str2))
+{
+
+	totalpve=1;
+	totalnve=1;
+	
+	vector<string> word=split(str2);
+	for(int j=0;j<word.size();j++){
+		bool flag=0;
+
+		for(int i=0;i<voc.size();i++)
+		{
+			if(voc[i].compare(word[j])==0)
+			{
+				double positiveProb=((positiveCounts[i]+1)*1.0)/(p+vocsize);
+				double negativeProb=((negativeCounts[i]+1)*1.0)/(n+vocsize);
+				totalpve*=positiveProb;
+				totalnve*=negativeProb;
+
+				flag=1;
+
+				break;		
+			}
+		}
+		if(flag==0)
+		{
+			double positiveProb=(1*1.0)/(p+vocsize);
+			double negativeProb=(1*1.0)/(n+vocsize);
+			totalpve*=positiveProb;
+			totalnve*=negativeProb;
+
+			
+		}
+		
+		
+	}
+	
+	
+		
+	if(totalpve>=totalnve && str2[0]=='+')
+		correct++;
+	else if(totalpve<totalnve && str2[0]=='-')
+		correct++;
+	else
+		wrong++;
+	
+
+}	  
+cout<<"Correct Prediction: "<<correct<<endl;
+cout<<"Wrong Prediction: "<<wrong<<endl;
+cout<<"Accuracy of Multinomial Naive Bayes Model: "<<(double)correct/(correct+wrong)*100<<"%"<<endl;
+return 0;
+}
